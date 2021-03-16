ec71a00b668a3d5143cce74aab61d146a4974448
Patrick Rebsch <prebsch@execonline.com>
2021-03-16 16:57:03 -0400

    Fix main file pointer

--- rollup.config.js
+++ rollup.config.js
@@ -20,7 +20,7 @@ const external = id => !id.startsWith('.') && !id.startsWith('/') && !id.include
 export default [
   {
     input,
-    output: { format: 'cjs', file: pkg.main },
+    output: { format: 'cjs', file: `dist/cjs/${name}.js` },
     external,
     plugins: [
       babel(getBabelOptions({ useESModules: false }))
